8914db5987c622aa99ccdb1b2a6581eaeeb35523
Roman Alikevich <roma.alikevich8@gmail.com>
2023-10-02 17:13:37 +0300

    completed task1

--- task1/ContentView.swift
+++ task1/ContentView.swift
@@ -9,18 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            GeneralView()
+            .tabItem {
+                Image(systemName: "star.fill")
+                Text("First")
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct BottomView: View {
+    var body: some View {
+        VStack {
+            Color.red
+                 .frame(height: 50)
+                 .opacity(0.8)
+                 .padding(.top, 0)
+        }.background(Color.clear)
+    }
+}
+
+struct GeneralView: View {
+    var body: some View {
+        VStack {
+            ScrollView {
+                ForEach(0..<20) { index in
+                    Text("Item \(index)")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .cornerRadius(10)
+                }
+                
+            }.safeAreaInset(edge: .bottom) {
+                BottomView().opacity(0.8)
+            }
+        }
     }
 }
